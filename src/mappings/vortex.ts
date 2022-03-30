d73dd7b478b6991234b99e6a012a85c281c51b9e
Shahzaib Habib <code.shahzaibhabib@gmail.com>
2022-03-30 12:56:32 +0500

    Implement vortex mappings

--- src/mappings/vortex.ts
+++ src/mappings/vortex.ts
@@ -1,4 +1,28 @@
 import { PortalCreated } from "../../generated/Vortex/Vortex";
 import { Vortex, Portal } from "../../generated/schema";
 
-export function handlePortalCreated(event: PortalCreated): void {}
+export function handlePortalCreated(event: PortalCreated): void {
+  let vortex = Vortex.load(event.address.toHexString());
+
+  if (!vortex) {
+    vortex = new Vortex(event.address.toHexString());
+    vortex.portals = [];
+  }
+
+  let portal = Portal.load(event.params.portal.toHexString());
+
+  if (!portal) {
+    portal = new Portal(event.params.portal.toHexString());
+    portal.creator = event.params.creator.toHexString();
+  }
+
+  portal.save();
+
+  const portals = vortex.portals;
+  if (portals) {
+    portals.push(portal.id);
+    vortex.portals = portals;
+  }
+
+  vortex.save();
+}
